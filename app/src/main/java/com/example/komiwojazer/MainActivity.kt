9e488b29ebf370e27778318a294f97b259a9e1c2
miamilemon <kontakt.thexenonz@gmail.com>
2022-11-29 22:12:28 +0100

    spinnery wypełniają się elementami ze strings.xml

--- app/src/main/java/com/example/komiwojazer/MainActivity.kt
+++ app/src/main/java/com/example/komiwojazer/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.komiwojazer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 /*
 Do zrobienia(plan projektu):
@@ -21,5 +23,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val spinnerA : Spinner = findViewById(R.id.spinnerA)
+        val spinnerB : Spinner = findViewById(R.id.spinnerB)
+        //To coś zapełnia spinnera elementami ze strings.xml
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.miasta,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerA.adapter = adapter
+            spinnerB.adapter = adapter
+        }
+
     }
 }
