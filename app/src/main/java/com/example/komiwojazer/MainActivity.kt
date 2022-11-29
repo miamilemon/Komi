f8c1c37c9f16255f150bdb54d3b0c2e79af5fe63
miamilemon <kontakt.thexenonz@gmail.com>
2022-11-29 21:44:26 +0100

    plan projektu w MainActivity.kt

--- app/src/main/java/com/example/komiwojazer/MainActivity.kt
+++ app/src/main/java/com/example/komiwojazer/MainActivity.kt
@@ -3,6 +3,20 @@ package com.example.komiwojazer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+Do zrobienia(plan projektu):
+- Design [X]
+- Miasta w spinnerze [ ]
+- Macierz wypełniana trójkątem który powoduje, że odległości są takie same z miasta A do B i z B do A [ ]
+- Wykrywanie zaznaczonych elementów na spinnerze [ ]
+- Pokazywanie odległości przy zaznaczeniu miast [ ]
+- Gdy są dwa te same miasta to nie można zmienić odległości [ ]
+- Zmiana odległości według zasad działania [ ]
+- Zrzut ekranu zapisywany do galerii [ ]
+- Algorytm, którego funkcjonalność działa [ ]
+ */
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
